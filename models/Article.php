ad4043012371992b1f618d8df3ebb653ccdc2659
roy <rxlisbest@163.com>
2017-10-24 15:44:42 +0800

    修改排序号model

--- models/Article.php
+++ models/Article.php
@@ -31,7 +31,7 @@ class Article extends \yii\db\ActiveRecord
 	public function rules()
 	{
 		return [
-			[['user_id', 'file_id', 'category_id', 'status'], 'integer'],
+			[['user_id', 'file_id', 'category_id', 'status', 'sort'], 'integer'],
 			[['content', 'cover_src'], 'string'],
 			[['title'], 'string'],
 			[['create_time', 'update_time'], 'integer']
@@ -52,7 +52,8 @@ class Article extends \yii\db\ActiveRecord
 			'category_id' => '分类ID',
 			'status' => '状态',
 			'create_time' => '创建时间',
-			'update_time' => '编辑时间'
+			'update_time' => '编辑时间',
+			'sort' => '排序号'
 		];
 	}
 }
